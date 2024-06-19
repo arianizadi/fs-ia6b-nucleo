99ef7729794b0184e8c147037d1d5e50665c5377
Arian Izadi <15961653+arianizadi@users.noreply.github.com>
2024-06-18 19:08:01 -0700

    Flash LED + Update to latest HAL

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,36 @@
 #![no_std]
 #![no_main]
 
-use cortex_m_semihosting::hprintln;
+// use cortex_m_semihosting::hprintln;
 // pick a panicking behavior
 use panic_halt as _; // you can put a breakpoint on `rust_begin_unwind` to catch panics
-// use panic_abort as _; // requires nightly
-// use panic_itm as _; // logs messages over ITM; requires ITM support
-// use panic_semihosting as _; // logs messages to the host stderr; requires a debugger
+                     // use panic_abort as _; // requires nightly
+                     // use panic_itm as _; // logs messages over ITM; requires ITM support
+                     // use panic_semihosting as _; // logs messages to the host stderr; requires a debugger
 
 use cortex_m_rt::entry;
+use embedded_hal::digital::InputPin;
+use embedded_hal::digital::OutputPin;
+use stm32f4xx_hal::pac::Peripherals;
+use stm32f4xx_hal::prelude::*;
 
 #[entry]
 fn main() -> ! {
-    hprintln!("Hello, world!").unwrap();
+    if let Some(peripherals) = Peripherals::take() {
+        let gpioa = peripherals.GPIOA.split(); 
 
-    loop {
-        // your code goes here
+        let mut led = gpioa.pa5.into_push_pull_output();
+
+        let gpioc = peripherals.GPIOC.split();
+        let button = gpioc.pc13;
+
+        loop {
+            if button.is_high() {
+                led.set_low();
+            } else {
+                led.set_high();
+            }
+        }
     }
+    loop {}
 }
