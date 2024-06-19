61eed8026624fa6a7d3c937ba4be563bbf80abaa
Arian Izadi <15961653+arianizadi@users.noreply.github.com>
2024-06-19 16:38:15 -0700

    Output to serial port

--- src/main.rs
+++ src/main.rs
@@ -3,35 +3,53 @@
 
 // use cortex_m_semihosting::hprintln;
 // pick a panicking behavior
-use panic_halt as _; // you can put a breakpoint on `rust_begin_unwind` to catch panics
-                     // use panic_abort as _; // requires nightly
-                     // use panic_itm as _; // logs messages over ITM; requires ITM support
-                     // use panic_semihosting as _; // logs messages to the host stderr; requires a debugger
+use panic_halt as _;
+use stm32f4xx_hal::uart::Config;
+// use stm32f4xx_hal::uart::Serial; // you can put a breakpoint on `rust_begin_unwind` to catch panics
+// use panic_abort as _; // requires nightly
+// use panic_itm as _; // logs messages over ITM; requires ITM support
+// use panic_semihosting as _; // logs messages to the host stderr; requires a debugger
 
+use core::fmt::Write;
 use cortex_m_rt::entry;
 use stm32f4xx_hal::pac::Peripherals;
 use stm32f4xx_hal::prelude::*;
-use core::fmt::Write;
 
 #[entry]
 fn main() -> ! {
-    if let Some(peripherals) = Peripherals::take() {
-        let gpioa = peripherals.GPIOA.split();
-        let gpioc = peripherals.GPIOC.split();
+    let dp = Peripherals::take().expect("cannot take peripherals");
 
-        let mut led = gpioa.pa5.into_push_pull_output();
-        let button = gpioc.pc13;
-        let mut led2 = gpioa.pa8.into_push_pull_output();
+    let gpioa = dp.GPIOA.split();
 
-        loop {
-            if button.is_high() {
-                led.set_low();
-                led2.set_low();
-            } else {
-                led.set_high();
-                led2.set_high();
-            }
-        }
+    let rcc = dp.RCC.constrain();
+
+    let clocks = rcc.cfgr.use_hse(8.MHz()).freeze();
+
+    let mut delay = dp.TIM1.delay_ms(&clocks);
+
+    // define RX/TX pins for usb serial
+    let tx_pin = gpioa.pa2.into_alternate();
+
+    // configure serial
+
+    // use the USART2 peripheral
+    let mut tx = dp
+        .USART2
+        .tx(tx_pin, Config::default().baudrate(9600.bps()), &clocks)
+        .unwrap();
+
+    let mut value: u8 = 0;
+
+    let mut led = gpioa.pa8.into_push_pull_output();
+
+    led.set_low();
+
+    loop {
+        led.toggle();
+        // print some value every 500 ms, value will overflow after 255
+        writeln!(tx, "value: {value:02}\r").unwrap();
+        // transmit the value
+        value = value.wrapping_add(1);
+        delay.delay(250.millis());
     }
-    loop {}
 }
