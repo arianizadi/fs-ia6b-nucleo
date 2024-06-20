32667cf448ec7631ee5c213341c6a2822d96ebe0
Arian Izadi <15961653+arianizadi@users.noreply.github.com>
2024-06-20 15:04:15 -0700

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,8 @@
 #![no_std]
 #![no_main]
 
-// use cortex_m_semihosting::hprintln;
-// pick a panicking behavior
 use panic_halt as _;
 use stm32f4xx_hal::uart::Config;
-// use stm32f4xx_hal::uart::Serial; // you can put a breakpoint on `rust_begin_unwind` to catch panics
-// use panic_abort as _; // requires nightly
-// use panic_itm as _; // logs messages over ITM; requires ITM support
-// use panic_semihosting as _; // logs messages to the host stderr; requires a debugger
-
 use core::fmt::Write;
 use cortex_m_rt::entry;
 use stm32f4xx_hal::pac::Peripherals;
@@ -27,12 +20,8 @@ fn main() -> ! {
 
     let mut delay = dp.TIM1.delay_ms(&clocks);
 
-    // define RX/TX pins for usb serial
     let tx_pin = gpioa.pa2.into_alternate();
 
-    // configure serial
-
-    // use the USART2 peripheral
     let mut tx = dp
         .USART2
         .tx(tx_pin, Config::default().baudrate(9600.bps()), &clocks)
